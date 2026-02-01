79f0b6e3b3988bb09e3209e6d173c3f6491d94da
thinkgo <thinkgo@aliyun.com>
2026-02-01 11:12:37 +0800

    fix: fix mix roi with logo

--- Opencv/run-over-cv/examples/09-mixture-roi.rs
+++ Opencv/run-over-cv/examples/09-mixture-roi.rs
@@ -3,52 +3,45 @@ use opencv::imgcodecs;
 use opencv::imgproc;
 use opencv::prelude::*;
 
-// #! 拉位操作
-// img1 = cv2.imread('assets/lena.png')
-// logo = cv2.imread('assets/logo.png')
-
-// # 把log放在左上角, 只关心这一区域
-// rows, cols = logo.shape[:2]
-
-// # 创建掩膜
-// logo2gray = cv2.cvtColor(logo,cv2.COLOR_BGR2GRAY)
-// ret, mask = cv2.threshold(logo2gray, 10, 255, cv2.THRESH_BINARY)
-// mask_inv = cv2.bitwise_not(mask)
-
-// # 只对roi区域进行融合
-// roi = img1[:rows, :cols]
-// img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
-// dst = cv2.add(img1_bg, logo)  # 进行融合
-// img1[:rows, :cols] = dst  # 融合后放在原图上
-// cv2.imwrite('assets/output/lena_mixture_logo.png', img1)
 fn main() -> Result<(), anyhow::Error> {
-    let img = imgcodecs::imread("assets/lena.png", imgcodecs::IMREAD_COLOR)?;
-    let logo = imgcodecs::imread("assets/logo.png", imgcodecs::IMREAD_GRAYSCALE)?;
-
-    // 把log放在左上角, 只关心这一区域
-    let rows = logo.rows();
-    let cols = logo.cols();
-
-    // 创建掩膜
+    // 处理logo
+    let logo = imgcodecs::imread("assets/logo.png", imgcodecs::IMREAD_COLOR)?;
+    let logo_rows = logo.rows();
+    let logo_cols = logo.cols();
+    // logo灰度图
+    let mut logo_gray = Mat::default();
+    imgproc::cvt_color(&logo, &mut logo_gray, imgproc::COLOR_BGR2GRAY, 0)?;
+    // logo块阈值处理, 生成掩膜
     let mut mask = Mat::default();
-    imgproc::threshold(&logo, &mut mask, 10.0, 255.0, imgproc::THRESH_BINARY)?;
+    imgproc::threshold(&logo_gray, &mut mask, 10.0, 255.0, imgproc::THRESH_BINARY)?;
     let mut mask_inv = Mat::default();
     core::bitwise_not(&mask, &mut mask_inv, &core::no_array())?;
-    let mut mask_inv_3ch = Mat::default();
-    imgproc::cvt_color(&mask_inv, &mut mask_inv_3ch, imgproc::COLOR_GRAY2BGR, 0)?;
 
-    // # 只对roi区域进行融合
-    let roi = img.roi(Rect {
+    // 处理原图
+    let mut img = imgcodecs::imread("assets/lena.png", imgcodecs::IMREAD_COLOR)?;
+    let mut roi = img.roi_mut(Rect {
         x: 0,
         y: 0,
-        width: rows,
-        height: cols,
+        width: logo_cols,
+        height: logo_rows,
     })?;
-    let mut roi_dst = Mat::default();
-    core::bitwise_and(&roi, &mask_inv_3ch, &mut roi_dst, &core::no_array())?;
-    let mut dest = Mat::default();
-    core::add(&logo, &roi_dst, &mut dest, &core::no_array(), -1)?;
+    // 保留原图除logo外的背景
+    // NOTE: 这样做的原因是logo有可能是透明的, 透明部分不应该被替换.
+    let mut roi_bg = Mat::default();
+    core::bitwise_and(&roi, &roi, &mut roi_bg, &mask_inv)?;
 
-    imgcodecs::imwrite("assets/output/lena_mixture_logo.png", &dest, &Vector::new())?;
+    // 图片叠加, 相加的两幅图片的形状必须相同.
+    let mut roi_mix_logo = Mat::default();
+    core::add(
+        &roi_bg,               // 输入图1
+        &logo,                 // 输入图2
+        &mut roi_mix_logo, // 输出图
+        &core::no_array(),     // 可选操作掩码
+        -1,                    // 可选深度
+    )?;
+    // 只改变roi区域, 也就是logo位
+    roi_mix_logo.copy_to(&mut roi)?;
+
+    imgcodecs::imwrite("assets/output/lena_mixture_logo.png", &img, &Vector::new())?;
     Ok(())
 }
